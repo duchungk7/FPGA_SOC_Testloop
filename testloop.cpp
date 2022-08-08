6f699d41f4f79c65f6647ed894dbb610c9428042
MP <duchungk7@gmail.com>
2022-08-08 15:41:44 +0800

    Add files via upload

---
+++ testloop.cpp
@@ -0,0 +1,15 @@
+#include "testloop.h"
+
+void testloop(din_t A, dout_t& X) {
+#pragma HLS INTERFACE ap_ctrl_none port=return
+//No control signals of block-level IO
+#pragma HLS INTERFACE s_axilite port= A
+#pragma HLS INTERFACE s_axilite port= X
+int i,j,k;
+dout_t accum=0;
+	for(i=0;i<A;i++)
+		for(j=0;j<A;j++)
+			for(k=0;k<A;k++)
+				accum += k-j*3+i*3;
+	X = accum;
+}
---
+++ testloop.h
@@ -0,0 +1,7 @@
+#include "ap_cint.h"
+//use arbitrary precision integer data types in a C function
+
+typedef int din_t;
+typedef int dout_t;
+
+void testloop(din_t A, dout_t& X);
